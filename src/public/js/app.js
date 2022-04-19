e778658ae4c9144d52474fdfd9e5ee180143cbd6
KJH <stonehead334@gmail.com>
2022-04-19 20:43:11 +0900

    Chat Complete

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 //front와 back에서 메시지를 주고 받을 수 있다
 
@@ -14,6 +16,11 @@ socket.addEventListener("close", () => {
   console.log("Disconnected from Server ❌");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 10000);
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value); //server로 메시지 보내고
+  input.value = ""; //보내지면 칸을 지운다
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -17,13 +17,17 @@ const server = http.createServer(app);
 //현재 http, webSocket를 같은 서버 동시에 실행시키고 있다, 굳이 안해도됨
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", (socket) => {
-    console.log("Connected to Browser ✅");
-    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-    socket.on("message", (message) => {
-      console.log(message);
-    });
-    socket.send("hello!!!");
-  });
+const sockets = [];
+
+wss.on("connection", socket => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", message => {
+    sockets.forEach(aSocket => aSocket.send(message.toString()));
+  }); //서로 다른 브라우저에 같은 메시지를 보냈다
+});
+
+//지금 서로 다른 브라우저에서 소통을 할거다. 어떤 연결된 유저가 하는지 알려줘야함
 
 server.listen(3000, handleListen);
