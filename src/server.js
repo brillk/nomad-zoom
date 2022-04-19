d73e7a22a8906101375f10fe5c2004dee24394fd
KJH <stonehead334@gmail.com>
2022-04-19 17:49:11 +0900

    WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
@@ -6,6 +8,13 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+
+//현재 http, webSocket를 같은 서버 동시에 실행시키고 있다, 굳이 안해도됨
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
